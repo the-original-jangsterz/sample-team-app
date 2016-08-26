552e1a0e19c9f12a76e73a28ad9c77dad57050ac
AriSendic <rabbisendic@gmail.com>
2016-08-25 20:19:55 -0500

    named penguin

---
+++ app/helpers/penguin_helper.rb
@@ -0,0 +1,2 @@
+module PenguinHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/penguin' => 'penguin#index'
+
   get '/' => 'pages#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
