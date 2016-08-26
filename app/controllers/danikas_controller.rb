ccafcf1bea91ad7eb3ea872e620a60e8c159584b
Danika <dazcha@hotmail.com>
2016-08-25 20:03:45 -0500

    Added Danika's controller, route and view

---
+++ app/controllers/danikas_controller.rb
@@ -0,0 +1,4 @@
+class DanikasController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/danikas' => 'danikas#index'
+
   get '/' => 'pages#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
