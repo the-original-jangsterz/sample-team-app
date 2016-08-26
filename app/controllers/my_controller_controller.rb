07da4c9bfd1cc3ebd935725e198a424ebb392add
David Basaraba <d.basaraba@gmail.com>
2016-08-25 20:39:00 -0500

    Did some stuff to this project

---
+++ app/controllers/my_controller_controller.rb
@@ -0,0 +1,5 @@
+class MyControllerController < ApplicationController
+  def index
+    render 'index.html.erb'
+  end
+end
---
+++ test/controllers/my_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MyControllerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
